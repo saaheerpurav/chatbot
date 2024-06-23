3eab9bddb2a12f3ede8b25c0eb00bc6de3b51d63
Saaheer <saaheer.purav@gmail.com>
2024-06-24 00:12:30 +0530

    Vercel update 5

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, request, jsonify, render_template, send_from_directory, session, cli
+from flask import (
+    Flask,
+    request,
+    jsonify,
+    render_template,
+    send_from_directory,
+    session,
+    cli,
+)
 import os
-import json
-import s3fs
 from waitress import serve
-#from paste.translogger import TransLogger
+
+# from paste.translogger import TransLogger
 
 from llama_index.legacy import (
     VectorStoreIndex,
@@ -12,10 +19,8 @@ from llama_index.legacy import (
     ServiceContext,
     load_index_from_storage,
 )
-
-#pipenv install -r requirements.txt
-# from llama_index.legacy.evaluation import FaithfulnessEvaluator
-from llama_index.legacy.llms.openai import OpenAI
+from llama_index.legacy.vector_stores.pinecone import PineconeVectorStore
+from pinecone import Pinecone, ServerlessSpec
 
 cli.load_dotenv()
 
@@ -23,64 +28,45 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 # ============ SET dev OR prod ============
-app.config["ENV"] = "prod"
+app.config["ENV"] = "dev"
 # =========================================
 
-s3 = s3fs.S3FileSystem(
-    key=os.environ.get("AWS_KEY"),
-    secret=os.environ.get("AWS_SECRET"),
-    s3_additional_kwargs={"ACL": "public-read"},
-)
-
-# evaluator, query_engine = None, None
+pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
 query_engine = None
 
 
-
 def initBot(bot_id):
-    PERSIST_DIR = "saaheer-chatbot-db/" + bot_id
     bot_name = "Saaheer Purav"
+    idx_list = list(pc.list_indexes().__dict__["index_list"]["indexes"])
+    exists = False
 
-    if not s3.exists(PERSIST_DIR):
-        # NEW
-        print("NEW")
-        documents = SimpleDirectoryReader("client_data/" + bot_id).load_data()
-        service_context = ServiceContext.from_defaults(
-            llm=OpenAI(temperature=0.3, model_name="gpt-3.5-turbo-0125")
-        )
-        # ServiceContext.from_defaults(chunk_size_limit=512),
-        index = VectorStoreIndex.from_documents(
-            documents, service_context=service_context
-        )
-        index.storage_context.persist(PERSIST_DIR, fs=s3)
-        session["bot_name"] = bot_name
-
-        with s3.open(PERSIST_DIR + "/config.json", "w") as f:
-            json.dump(
-                {
-                    "id": bot_id,
-                    "bot_name": bot_name,
-                },
-                f,
-            )
+    for i in idx_list:
+        if bot_id in i.__dict__["_data_store"].values():
+            exists = True
+    
+    if exists is True:
+        print("EXISTS")
+        pinecone_index = pc.Index(bot_id)
+        vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+        index = VectorStoreIndex.from_vector_store(vector_store)
+    
     else:
-        # EXISTING
-        print("EXISTING")
-        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR, fs=s3)
-        index = load_index_from_storage(storage_context)
-
-        with s3.open(PERSIST_DIR + "/config.json", "r") as f:
-            session["bot_name"] = json.load(f)["bot_name"]
-
-    """service_context = ServiceContext.from_defaults(
-        llm=OpenAI(temperature=0.3, model_name="gpt-3.5-turbo-0125")
-    )
-
-    global evaluator
-    evaluator = FaithfulnessEvaluator(service_context=service_context)"""
+        print("NEW")
+        pc.create_index(
+            name=bot_id,
+            dimension=1536,
+            metric="euclidean",
+            spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+        )
+        pinecone_index = pc.Index(bot_id)
+        vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        documents = SimpleDirectoryReader("client_data/" + bot_id).load_data()
+        index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
 
     global query_engine
     query_engine = index.as_query_engine()
+    session["bot_name"] = bot_name
 
 
 
@@ -93,11 +79,9 @@ def init_llm():
     return jsonify({"status": 200, "bot_name": session["bot_name"]})
 
 
-
 @app.route("/chatbot-iframe")
 def main():
     return render_template("index.html")
-
 
 
 @app.route("/chatbot", methods=["POST"])
@@ -111,23 +95,11 @@ def chatbot():
 
     return jsonify({"response": str(response)})
 
-    """eval_result = evaluator.evaluate_response(response=response)
-
-    if eval_result.passing:
-        return jsonify({"response": str(response)})
-    else:
-        return jsonify(
-            {
-                "response": "I'm sorry as I can only answer questions relating to Saaheer Purav."
-            }
-        )"""
-
 
 
 @app.route("/static/css")
 def send_css():
     return send_from_directory("static" + os.sep + "css", "output.css")
-
 
 
 if __name__ == "__main__":
@@ -136,4 +108,4 @@ if __name__ == "__main__":
 
     elif app.config["ENV"] == "prod":
         serve(app, host="0.0.0.0", port=8080)
-        #serve(TransLogger(app, setup_console_handler=False), host="0.0.0.0", port=8080)
+        # serve(TransLogger(app, setup_console_handler=False), host="0.0.0.0", port=8080)
